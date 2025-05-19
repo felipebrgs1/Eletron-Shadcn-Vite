672aeff76b29353a435ee844dad4f425a2a68436
felipeb <felipeborgaco@hotmail.com>
2025-05-18 21:27:38 -0300

    Add tanstack router

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 
 export default defineConfig({
     main: {
@@ -16,6 +17,10 @@ export default defineConfig({
                 '@': resolve('src/renderer/src'),
             },
         },
-        plugins: [react(), tailwindcss()],
+        plugins: [
+            TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+            react(),
+            tailwindcss(),
+        ],
     },
 });
